9396177f3e1a62c20938612c79d4a3557b604a53
Yasuhito Nagatomo <ynaga@Yasuhitos-Mac-mini.local>
2024-11-10 11:41:55 +0900

    Add a rocket

--- FlyingToyRocket/AppModel/AppModel.swift
+++ FlyingToyRocket/AppModel/AppModel.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import RealityKit
 
 /// Maintains app-wide state
 @MainActor
@@ -18,4 +19,26 @@ class AppModel {
         case open
     }
     var immersiveSpaceState = ImmersiveSpaceState.closed
+
+    // Immersive Scene Animation
+
+    private var accumulativeTime: Double = 0
+    private var rocketEntity: Entity!
+    private var rocketVelocity: SIMD3<Float> = .zero
+}
+
+// MARK: - Immersive Scene Animation
+
+extension AppModel {
+    func setupFlyingScene(rocketEntity: Entity) {
+        self.rocketEntity = rocketEntity
+        rocketEntity.position = .zero
+        self.rocketVelocity = .init(0, 0.01, 0)
+        self.accumulativeTime = 0
+    }
+
+    func doAnimation(_ deltaTime: Double) {
+        accumulativeTime += deltaTime
+        rocketEntity?.position += rocketVelocity * Float(accumulativeTime)
+    }
 }
--- FlyingToyRocket/Views/ImmersiveView.swift
+++ FlyingToyRocket/Views/ImmersiveView.swift
@@ -8,20 +8,38 @@
 import SwiftUI
 import RealityKit
 import RealityKitContent
+// import Combine
 
 struct ImmersiveView: View {
+    @Environment(AppModel.self) private var appModel
+    @State private var eventSubscription: EventSubscription?
 
     var body: some View {
         RealityView { content in
-            // Add the initial RealityKit content
-            if let immersiveContentEntity = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
-                content.add(immersiveContentEntity)
+            if let rootEntity = try? await Entity(named: "Fly", in: realityKitContentBundle) {
+                rootEntity.position = .init(x: 0, y: 0, z: -1.5)
 
-                // Put skybox here.  See example in World project available at
-                // https://developer.apple.com/
+                if let rocket = rootEntity.findEntity(named: "ToyRocket") {
+                    appModel.setupFlyingScene(rocketEntity: rocket)
+                    eventSubscription = content.subscribe(to: SceneEvents.Update.self) { event in
+                        appModel.doAnimation(event.deltaTime)
+                    } // subscription
+                } else {
+                    assertionFailure()
+                } // if
+
+                content.add(rootEntity)
+                // Add an Sky-dome
+                // Add an IBL
+            } else {
+                assertionFailure()
             }
+        } // RealityView
+        .onDisappear {
+            eventSubscription?.cancel() // fail-safe
+            eventSubscription = nil
         }
-    }
+    } // body
 }
 
 #Preview(immersionStyle: .mixed) {
